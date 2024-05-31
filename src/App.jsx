921ca257a1f8a6d61e3e83e1c6396dab801e4d8c
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-05-31 10:41:18 +0700

    feat: add feature add data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react';
 import './components/ProductCard.css';
 import ProductList from './components/ProductList';
 import { Products } from './data/Product';
+import ProductCreate from './components/ProductCreate';
 
 function App() {
   const [products, setProducts] = useState(Products)
 
+  const onCreateProduct = (product) => {
+    // console.log('Data parent: ', product);
+    setProducts([...products, {id: Math.round(Math.random() * 7777), ...product} ])
+  }
+
   return (
     <>
       <div className="app-title">Belanjda Mobil </div>
-      <ProductList products= { products} />
+      <ProductCreate onCreateProduct = {onCreateProduct} />
+      <ProductList products= { products } />
     </>
   );
 }
---
+++ src/components/ProductCreate.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './ProductCard.css';
+
+const ProductCreate = (props) => {
+    const {onCreateProduct} = props
+
+  const initialState = {
+    nama: '',
+    deskripsi: '',
+    imageURL: '',
+  };
+
+  const [showForm, setShowForm] = useState(false);
+  const [formData, setFormData] = useState(initialState);
+  const { nama, deskripsi, imageURL } = formData;
+
+  const handleShow = () => {
+    setShowForm(!showForm);
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    onCreateProduct(formData)
+    setFormData(initialState);
+  };
+
+  return (
+    <div className="product-create">
+      <div className="toggle-add">
+        <button className="edit-input-submit add-toggle" onClick={handleShow}>
+          {showForm ? 'Close Form' : 'Add Product'}
+        </button>
+      </div>
+
+      {showForm && (
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-add-title ">Add Product</div>
+          <div className="form-group">
+            <input type="text" className="add-input-text" name="nama" value={nama} onChange={handleChange} placeholder="Nama product" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="add-input-text" name="deskripsi" value={deskripsi} onChange={handleChange} placeholder="Deskripsi Product" />
+          </div>
+          <div className="form-group">
+            <input type="text" className="add-input-text" name="imageURL" value={imageURL} onChange={handleChange} placeholder="Image Product" />
+          </div>
+          <input type="submit" className="edit-input-submit add" />
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default ProductCreate;
