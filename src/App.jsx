afb8eccadc6b87edc5f78d3ae68e67027293b6b3
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-07 10:54:51 +0700

    ref: refactor crud with json server

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './components/ProductCard.css';
 import ProductList from './components/ProductList';
-import { Products } from './data/Product';
 import ProductCreate from './components/ProductCreate';
+import axios from 'axios';
+import { BASE_URL } from './constant/BASE_URL';
 
 function App() {
-  const [products, setProducts] = useState(Products);
+  const [products, setProducts] = useState([]);
 
-  const onCreateProduct = (product) => {
-    setProducts([...products, { id: Math.round(Math.random() * 7777), ...product }]);
+  //? Nampilin product
+  const fetchProduct = async () => {
+    const response = await axios.get(BASE_URL);
+    setProducts(response.data);
   };
 
-  const onDeleteProduct = (id) => {
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  //? Create Product
+  const onCreateProduct = async (product) => {
+    const response = await axios.post(BASE_URL, product);
+
+    setProducts([...products, response.data]);
+  };
+
+  //? Delete Product
+  const onDeleteProduct = async (id) => {
+    await axios.delete(`${BASE_URL}/${id}`);
+
     const updatedProduct = products.filter((prod) => {
       return prod.id !== id;
     });
     setProducts(updatedProduct);
   };
 
-  const onSaveEdit = (id, updatedData) => {
-    const updatedProducts = products.map((prod) => 
-      prod.id === id ? { ...prod, ...updatedData } : prod
-    );
-    setProducts(updatedProducts);
+  //? Edit prodct
+  const onSaveEdit = async (id, updatedData) => {
+    const response = await axios.put(`${BASE_URL}/${id}`, updatedData);
+
+    const updatedProduct = products.map((prod) => {
+      if (prod.id === id) {
+        return {
+          ...prod,
+          ...response.data,
+        };
+      }
+
+      return prod
+    });
+
+    setProducts(updatedProduct)
   };
 
   return (
