fdca7b35574a2716186ed5e2931d947255ec9fb1
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-06-08 07:06:28 +0700

    feat: add product context

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import './components/ProductCard.css';
 import ProductList from './components/ProductList';
 import ProductCreate from './components/ProductCreate';
-import { createProductAPI, deleteProductAPI, editProductAPI, fetchProductAPI } from './api/ProductAPI';
+
 
 function App() {
-  const [products, setProducts] = useState([]);
 
-  //? Nampilin product
-  const fetchProduct = async () => {
-    const response = await fetchProductAPI();
-    setProducts(response.data);
-  };
 
   useEffect(() => {
     fetchProduct();
   }, []);
 
-  //? Create Product
-  const onCreateProduct = async (product) => {
-    const response = await createProductAPI(product);
-
-    setProducts([...products, response.data]);
-  };
-
-  //? Delete Product
-  const onDeleteProduct = async (id) => {
-    await deleteProductAPI(id);
-
-    const updatedProduct = products.filter((prod) => {
-      return prod.id !== id;
-    });
-    setProducts(updatedProduct);
-  };
-
-  //? Edit prodct
-  const onSaveEdit = async (id, updatedData) => {
-    const response = await editProductAPI(id, updatedData);
-
-    const updatedProduct = products.map((prod) => {
-      if (prod.id === id) {
-        return {
-          ...prod,
-          ...response.data,
-        };
-      }
-
-      return prod;
-    });
-
-    setProducts(updatedProduct);
-  };
+  
 
   return (
     <>
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useState } from 'react';
+import { createProductAPI, deleteProductAPI, editProductAPI, fetchProductAPI } from '../api/ProductAPI';
+
+const ProductContext = createContext();
+
+function Provider({ children }) {
+  const [products, setProducts] = useState([]);
+
+  //? Nampilin product
+  const fetchProduct = async () => {
+    const response = await fetchProductAPI();
+    setProducts(response.data);
+  }; 
+
+  //? Create Product
+  const onCreateProduct = async (product) => {
+    const response = await createProductAPI(product);
+
+    setProducts([...products, response.data]);
+  };
+
+  //? Delete Product
+  const onDeleteProduct = async (id) => {
+    await deleteProductAPI(id);
+
+    const updatedProduct = products.filter((prod) => {
+      return prod.id !== id;
+    });
+    setProducts(updatedProduct);
+  };
+
+  //? Edit prodct
+  const onSaveEdit = async (id, updatedData) => {
+    const response = await editProductAPI(id, updatedData);
+
+    const updatedProduct = products.map((prod) => {
+      if (prod.id === id) {
+        return {
+          ...prod,
+          ...response.data,
+        };
+      }
+
+      return prod;
+    });
+
+    setProducts(updatedProduct);
+  };
+
+  const valueToShare = {
+    products,
+    onCreateProduct,
+    onDeleteProduct,
+    onSaveEdit,
+    fetchProduct,
+  };
+
+  return (
+    <productContext.Provider value={valueToShare}>
+        {children}
+    </productContext.Provider>
+  )
+}
+
+
+export {Provider}
+export default ProductContext
